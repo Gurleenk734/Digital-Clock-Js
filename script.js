950f64a8e83c514818622aa90bb4d547bb1e6054
Gurleen Kaur <gurleenk0418@gmail.com>
2025-12-17 20:57:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+// creating variables
+
+let display = document.querySelector(".time");
+let startBtn = document.getElementById("start");
+let stopBtn = document.getElementById("stop");
+let resumeBtn = document.getElementById("resume");
+
+let timerID;
+let timerRunning = false;
+
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+
+const tickSound = new Audio("tick.wav");
+tickSound.volume = 0.1;
+
+//initialize clocks to 00:00:00
+function initializeClock() {
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+
+  display.textContent = "00:00:00";
+}
+
+//function to update clock every second
+function updateClock() {
+  seconds++;
+
+  if (seconds === 60) {
+    seconds = 0;
+    minutes++;
+  }
+
+  if (minutes === 60) {
+    minutes = 0;
+    hours++;
+  }
+
+  display.textContent =
+    String(hours).padStart(2, "0") +
+    ":" +
+    String(minutes).padStart(2, "0") +
+    ":" +
+    String(seconds).padStart(2, "0");
+
+  tickSound.currentTime = 0;
+  tickSound.play();
+}
+
+//start functionality
+function startTimer() {
+  if (timerRunning == false) {
+    timerID = setInterval(updateClock, 1000);
+    timerRunning = true;
+  }
+
+  return timerID;
+}
+
+//stop functionality
+function stopTimer() {
+  clearInterval(timerID);
+  timerRunning = false;
+  initializeClock();
+
+  resumeBtn.textContent = "Resume";
+}
+
+//resume&pause functionality
+function resumeTimer() {
+  if (timerRunning == true) {
+    clearInterval(timerID);
+    timerRunning = false;
+    resumeBtn.textContent = "Resume";
+  } else {
+    timerID = setInterval(updateClock, 1000);
+    timerRunning = true;
+    resumeBtn.textContent = "Pause";
+  }
+}
+
+//event listeners
+startBtn.addEventListener("click", startTimer);
+stopBtn.addEventListener("click", stopTimer);
+resumeBtn.addEventListener("click", resumeTimer);
